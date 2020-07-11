c78662e6f5b2ef0e0db855708b47ae83000fe439
pledgedavis <pledgedavis14@gmail.com>
2020-07-11 15:48:00 -0400

    db tables

---
+++ db/migrate/20200711194638_create_shoes.rb
@@ -0,0 +1,6 @@
+class CreateShoes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :shoes do |t|
+    end
+  end
+end
---
+++ db/migrate/20200711194744_create_brands.rb
@@ -0,0 +1,6 @@
+class CreateBrands < ActiveRecord::Migration[6.0]
+  def change
+    create_table :brands do |t|
+    end
+  end
+end
