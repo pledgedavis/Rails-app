1e2c6e273593c46d68996d14e17a848a04a0efec
pledgedavis <pledgedavis14@gmail.com>
2020-08-25 01:42:49 -0400

    login and sign up strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,14 @@ class UsersController < ApplicationController
 
 
  def create 
+    @user = User.new(user_params)
  end
 
+private 
 
+ def user_params
+    params.require(:user).permit(:username, :email, :password)
+ end
   
 
 end
