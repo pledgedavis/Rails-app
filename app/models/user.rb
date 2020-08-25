4855d347c6d2c22f87605976f990f8135edbd67d
pledgedavis <pledgedavis14@gmail.com>
2020-08-25 01:39:34 -0400

    login and sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :brands
     has_many :shoes
     has_many :brands, through: :shoes 
-    has_secure_password
+
+
+    validates :username, :email, presence: true 
+    validates :username, :email, uniqueness: true 
 
 end
