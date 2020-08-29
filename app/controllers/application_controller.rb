84bfa28f4fff768d2bb3d76df611a70c7b382142
pledgedavis <pledgedavis14@gmail.com>
2020-08-29 08:26:56 -0400

    current user method fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
 
  
  
-
+private 
   def current_user
-    session[:user_id]
+    # session[:user_id]
+   @current_user ||= User.find_by_id(session[:user_id])
   end
 end
