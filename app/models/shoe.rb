57aec64796d51eedddbd4d7ccd000713315d509c
pledgedavis <pledgedavis14@gmail.com>
2020-09-03 22:16:35 -0400

    adding validation for shoe price

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe < ApplicationRecord
     belongs_to :brand
     belongs_to :user
+    validates :retail_price, :resale_price, length: { minimum: 0, maximum: 9999}
     validates :retail_price, :resale_price, :quality, presence: true 
     validates :retail_price, :resale_price, :quality, uniqueness: true 
 end
