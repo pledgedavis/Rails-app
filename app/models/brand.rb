bd4baee429c66e84a84f7625f3ed33776e865e66
pledgedavis <pledgedavis14@gmail.com>
2020-09-06 01:37:13 -0400

    adding brand validations

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,6 @@
 class Brand < ApplicationRecord
     has_many :shoes
     # belongs_to :user, optional: true
+    validates :company_name, presence: true 
+    validates :company_name, uniqueness: true 
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,5 +3,7 @@ class Shoe < ApplicationRecord
     belongs_to :user
     validates :retail_price, :resale_price, length: { minimum: 0, maximum: 9999}
     validates :retail_price, :resale_price, :quality, presence: true 
-    validates :retail_price, :resale_price, :quality, uniqueness: true 
+      
+    accepts_nested_attributes_for :brand
+
 end
