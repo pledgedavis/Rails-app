89a67e95129d1062933c22bf82387508482c5880
pledgedavis <pledgedavis14@gmail.com>
2020-09-06 22:53:57 -0400

    fixing omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,21 +30,6 @@ class SessionsController < ApplicationController
 #     redirect_to '/'
 #   end 
 
-#   def fbcreate 
-#     @user = User.from_omniauth(auth)
-#     @user.save
-#     session[:user_id] = @user.id
-#    redirect_to user_path(@user)
-#     end
- 
-#   def auth
-#     request.env['omniauth.auth']
-#   end
-
-# end
-
-
-
 def create 
   @user = User.find_by(username: params[:user][:username])
 if @user && @user.authenticate(params[:user][:password])
