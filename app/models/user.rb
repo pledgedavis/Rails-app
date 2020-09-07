d08967be4ec33ad278dec4a07b0d926bd9a8d789
pledgedavis <pledgedavis14@gmail.com>
2020-09-06 22:53:10 -0400

    added users homepage and link to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,4 @@ class User < ApplicationRecord
     validates :username, :email, uniqueness: true 
     has_secure_password
 
-
-
-
-
-
-
-#     def self.from_omniauth(auth)
-#         where(email: auth.info.email).first_or_initialize do |user|
-#         user.username = auth.info.name
-#         user.email = auth.info.email
-#         user.password = SecureRandom.hex
-#      end
-#   end
 end
