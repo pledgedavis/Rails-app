1a4e8335c512fe2ce6febd9d98877bba828fad84
pledgedavis <pledgedavis14@gmail.com>
2020-09-16 12:29:10 -0400

    Added comments to brandscontroller

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -3,7 +3,9 @@ class BrandsController < ApplicationController
     before_action :check_if_logged_in
 
   def index 
+        #line does not execute if params does not contain a shoe_id
     if params[:shoe_id]
+      # binding.pry
         @brands = Shoe.find(params[:shoe_id]).brands
       else
     @brands = Brand.order_by_name
